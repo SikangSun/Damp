4cfd34503b4d1b8d8ea56ffb61d081ac07f75bcc
SikangSun <89489927+SikangSun@users.noreply.github.com>
2023-03-17 02:52:15 -0400

    last commit before deployment

--- src/database/index.ts
+++ src/database/index.ts
@@ -54,7 +54,7 @@ export const getQuote = async (input: string, guildID: string): Promise<QuoteDef
 
 export const updateTagQuote = async (identifier: string, newtag: string, input: string, guildID: string): Promise<boolean> => {
   const temp = await initializeCollection(guildID);
-  const discriminator = idOrTag(input) === "tag" ? {tag: input} : {id: input};
+  const discriminator = idOrTag(input) === "tag" ? {tag: input} : {id: Number(input)};
   let result = await temp.updateOne(discriminator, {$set: {tag: newtag}})
     .catch((err: any) => {console.log(err);return false;})
   return result.modifiedCount ? true : false;
